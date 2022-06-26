8cf7e71b710289ca1cb2d55b6439c9176d22dbf6
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2022-06-26 18:56:22 +0200

    Add GA script

---
+++ src/components/GoogleAnalytics.tsx
@@ -0,0 +1,23 @@
+import Script from 'next/script'
+
+export function GoogleAnalytics () {
+  return (
+    <>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id='google-analytics' strategy='lazyOnload'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+          });
+              `}
+      </Script>
+    </>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import type { AppProps } from 'next/app'
 
 import { DefaultSeo } from 'components/Seo'
-
+import { GoogleAnalytics } from 'components/GoogleAnalytics'
 import 'styles/globals.css'
 
 function MyApp ({ Component, pageProps }: AppProps) {
   return (
     <>
+      <GoogleAnalytics />
       <DefaultSeo />
       <Component {...pageProps} />
     </>
